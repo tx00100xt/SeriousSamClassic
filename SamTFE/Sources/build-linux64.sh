013c7187738fc9aab87a727add234026704dbcde
tx00100xt <t.x00100x.t@yandex.ru>
2022-07-06 16:22:27 +0300

    Fixed building scrips.

--- SamTFE/Sources/build-linux64.sh
+++ SamTFE/Sources/build-linux64.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 NCPU=`cat /proc/cpuinfo |grep vendor_id |wc -l`
-let NCPU=$NCPU
+
+if [[ $NCPU -eq 0 ]]
+then
+  let NCPU='4'
+else
+  let NCPU=$NCPU
+fi
+
 echo "Will build with 'make -j$NCPU' ... please edit this script if incorrect."
 
 set -e
